5cc617fb8a303f2f023a0a86271984f5bcebec22
thaiis-souto7 <dammascenothais@gmail.com>
2021-08-17 20:10:07 -0300

    New Player

--- python/BlackJack.py
+++ python/BlackJack.py
@@ -1,22 +1,52 @@
 # -*- coding: utf-8 -*-
-__author__ = "Filipe Ribeiro"
-
 import socket, sys
-
 
 HOST = '127.0.0.1'  # endereço IP
 PORT = 20000        # Porta utilizada pelo servidor
 BUFFER_SIZE = 1024  # tamanho do buffer para recepção dos dados
 
+Players = []
+
+class AddPlayer:
+    """User class for representing name, city, amount and victories of players"""
+    def _init_(self, amount, victories):
+        name_user = input('----> Digite seu nome: ')
+        city_user = input('----> Digite sua cidade: ')
+        """Create a new Player"""
+        self.name = name_user
+        self.city = city_user
+        self.amount = amount
+        self.victories = victories
+
+    def getName(self):
+        return self.name
+
+    def getCity(self):
+        return self.city
+
+    def getAmount(self):
+        return self.amount
+
+    def getVictories(self):
+        return self.victories
 
 def main(argv): 
+
+    users = [[]]
+
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
-            print("Servidor executando!")
+
+            print("\n*********** BLACKJACK ***********")
+            
+
             while(True):       
-                texto = input("Digite o texto a ser enviado ao servidor:\n")
-                s.send(texto.encode()) #texto.encode - converte a string para bytes
+                    
+                p1 = AddPlayer()
+                
+
+                #s.send(texto.encode()) #texto.encode - converte a string para bytes
                 data = s.recv(BUFFER_SIZE)
                 texto_recebido = repr(data) #converte de bytes para um formato "printável"
                 print('Recebido do servidor', texto_recebido)
@@ -25,6 +55,7 @@ def main(argv):
                     print('vai encerrar o socket cliente!')
                     s.close()
                     break
+
     except Exception as error:
         print("Exceção - Programa será encerrado!")
         print(error)
